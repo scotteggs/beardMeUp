dd0b3039b021ab622999be92ec7a8aa188efe621
Daniel Somekh <somekh.daniel@gmail.com>
2015-11-03 17:08:55 -0500

    Order test added

--- tests/server/models/order-test.js
+++ tests/server/models/order-test.js
@@ -1,67 +1,72 @@
-// var dbURI = 'mongodb://localhost:27017/testingDB';
-// var clearDB = require('mocha-mongoose')(dbURI);
+var dbURI = 'mongodb://localhost:27017/testingDB';
+var clearDB = require('mocha-mongoose')(dbURI);
 
-// var expect = require('chai').expect;
-// var mongoose = require('mongoose');
+var chai = require('chai');
+var expect = chai.expect;
+var mongoose = require('mongoose');
+var Promise = require('bluebird');
+var chaiAsPromise = require('chai-as-promised');
+chai.use(chaiAsPromise);
 
-// // Require in all models.
-// require('../../../server/db/models');
+// Require in all models.
+require('../../../server/db/models');
 
-// var Order = mongoose.model('Order');
-// var User = mongoose.model('User');
+var Order = mongoose.model('Order');
+var User = mongoose.model('User');
+var Product = mongoose.model('Product');
 
-// describe('Order model', function () {
+describe('Order model', function () {
 
-//     beforeEach('Establish DB connection', function (done) {
+    var product;
+    var order;
+    var user;
 
-//         if(!mongoose.connection.db) mongoose.connect(dbURI, done);
-//         var user = new User({
-//             email: "test email"
-//         })
+    beforeEach('Establish DB connection', function (done) {
 
-//         var order = new Order({
-//             user: 
-//             name: 'Tom Hank Beard',
-//             sku: 'mysku1',
-//             desc: 'my description',
-//             price: 1000,
-//             type: 'Beard',
-//             colorOptions: [{name: 'Blue', HSB:[0,0,0]}],
-//             active: true,
-//             tags: ['cool', 'sick']
-//         });
+        if(!mongoose.connection.db) mongoose.connect(dbURI);
+        var userPromise = User.create({
+            email: "mike@fullstack.com"
+        })
+        var productPromise = Product.create({
+            name: 'Tom Hank Beard',
+            sku: 'mysku1',
+            desc: 'my description',
+            price: 1000,
+            type: 'Beard',
+            colorOptions: [{name: 'Blue', HSB:[0,0,0]}],
+            tags: ['cool', 'sick']
+        });
 
-//         product.save()
-//         .then(done)
-//     });
+        Promise.all([userPromise, productPromise])
+        .then(function(all){
+            user = all[0]
+            product = all[1]
+            order = new Order({
+                user: user._id,
+                cart:[{qty:1,product: product._id,color: 'Blue'}]
+            });
+            return order.save()
+        })
+        .then(function(order){
+            order = order;
+            done();
+        })
+    });
 
-//     afterEach('Clear test database', function (done) {
-//         clearDB(done);
-//     });
+    afterEach('Clear test database', function (done) {
+        clearDB(done);
+    });
 
-//     it('should exist', function () {
-//         expect(Product).to.be.a('function');
-//     });
+    it('should exist', function () {
+        expect(Order).to.be.a('function');
+    });
 
-//     describe('product creation', function () {
-
-//         describe('single product', function () {
-
-//             it('should exist', function () {
-//                 expect(product.name).to.be('Tom Hank Beard');
-//                 expect(product.sku).to.be('mysku1');
-//                 expect(product.desc).to.be('my description');
-//                 expect(product.price).to.be(1000);
-//                 expect(product.type).to.be('Beard');
-//                 expect(product.colorOptions[0].name).to.be('Blue');
-//                 expect(product.colorOptions[0].stock).to.be(0);
-//                 expect(product.active).to.be(false);
-//                 expect(product.tags).to.be(['cool', 'sick']);
-//             });
+    it('should create an order with valid cart and user', function () {
+        expect(order.user).to.equal(user._id);
+        expect(order.cart[0].product).to.equal(product._id);
 
 
-//         });
+    });
 
-    
-//     });
-// });
+
+});
