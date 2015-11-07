55b0982c28df68fb0af285629cc76b39cafde567
lesterhm <lesterechem@gmail.com>
2015-11-07 10:31:20 -0500

    adding edit modal

--- browser/js/all-products/all-products-ctrl.js
+++ browser/js/all-products/all-products-ctrl.js
@@ -2,4 +2,7 @@ app.controller('AllProductsCtrl', function($scope, allProducts){
 	$scope.allProducts = allProducts;
 	$scope.orderFilter = "all";
 	$scope.addProduct = true;
+	$scope.editProduct = function(id){
+		console.log(id);
+	}
 })
