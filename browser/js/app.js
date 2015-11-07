51557848079a4ebc38b465f77da62fd0787ee4e6
Scott <scotteggs@gmail.com>
2015-11-07 15:30:59 -0500

    resolved merge conflicts

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,9 +1,7 @@
 'use strict';
-<<<<<<< HEAD
-window.app = angular.module('FullstackGeneratedApp', ['fsaPreBuilt', 'ui.router', 'ui.bootstrap', 'ngAnimate', 'ngSanitize','angularPayments']);
-=======
-window.app = angular.module('FullstackGeneratedApp', ['fsaPreBuilt', 'ui.router', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'ngMessages']);
->>>>>>> master
+
+window.app = angular.module('FullstackGeneratedApp', ['fsaPreBuilt', 'ui.router', 'ui.bootstrap', 'ngAnimate', 'ngSanitize','angularPayments', 'ngMessages']);
+
 
 app.config(function ($urlRouterProvider, $locationProvider) {
     // This turns off hashbang urls (/#about) and changes it to something normal (/about)
