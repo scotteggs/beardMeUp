68d05f95bb4d02e011f206566b9b3f34f34c1e04
Mike Ingber <mbingber@gmail.com>
2015-11-09 17:28:10 -0500

    buttons working in product view, cart view works

--- browser/js/common/directives/cart/cart-directive.js
+++ browser/js/common/directives/cart/cart-directive.js
@@ -28,6 +28,12 @@ app.directive('cart', function(UserFactory){
 				})
 				return total;
 			}
+
+			$scope.imageUrl = function(cartItem) {
+		        if (cartItem.color) {
+		            return cartItem.product.imageUrl.slice(0,-4) + '-' + cartItem.color + '.jpg';
+		        } else return cartItem.product.imageUrl;
+		    }
 		}
 	}
 })
