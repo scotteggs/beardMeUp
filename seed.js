5197865dcc2f8282edd38cd56de79e5b5571c979
lesterhm <lesterechem@gmail.com>
2015-11-10 13:49:05 -0500

    added routes for active products, modified models for active default, modified orderfactory

--- seed.js
+++ seed.js
@@ -87,33 +87,6 @@ var seedUsers = function (products) {
             }]
         },
         {
-            firstName: 'Beard',
-            lastName: 'Admin',
-            email: 'somekh.daniel@gmail.com',
-            password: 'password',
-            primaryAddress: [{
-                line1: '123 fullstack',
-                // line2: '',
-                city: 'New York',
-                state: 'NY',
-                zip: '42424',
-                phone: '555-555-5555'
-            }],
-            role: 'siteAdmin',
-            cart: [{
-                qty: 2,
-                product: products[0].id,
-                color: 'blue',
-                price: products[0].price
-            },
-            {
-                qty: 3,
-                product: products[1].id,
-                color: 'black',
-                price: products[1].price
-            }]
-        },
-        {
             firstName: 'Weirdo',
             lastName: 'Beardo',
             email: 'somekh.daniel@gmail.com',
