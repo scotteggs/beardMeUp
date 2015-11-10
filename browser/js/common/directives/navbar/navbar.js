a4aa1844f0b34e8e4ae4589f36a2e8700a07bcf8
Daniel Somekh <danielsomekh@Daniels-MacBook-Pro.local>
2015-11-10 17:20:15 -0500

    Sticky navbar implemented

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -58,12 +58,14 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state, 
             $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
             $rootScope.$on(AUTH_EVENTS.sessionTimeout, removeUser);
 
-            var num = 250;
+            var num = 230;
             $(window).bind('scroll', function() {
                 if($(window).scrollTop() > num) {
                     $('.nav-sticky').addClass('fixed');
                 } else {
-                    $('nav-sticky').removeClass('fixed');
+                    console.log("scroll less then num")
+                    $('.nav-sticky').removeClass('fixed');
+
                 }
             })
 
