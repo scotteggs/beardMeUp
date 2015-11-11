96dd491a9bacbcbefa947149a592f456d599a2cd
Scott <scotteggs@gmail.com>
2015-11-10 19:51:55 -0500

    changes not ready but commiting

--- browser/js/common/modals/user-orders/user-orders.js
+++ browser/js/common/modals/user-orders/user-orders.js
@@ -1,5 +1,17 @@
-app.controller('UserOrdersCtrl', function (theOrders, theUser, $scope) {
+app.controller('UserOrdersCtrl', function (ProductFactory, theOrders, theUser, $scope) {
 	$scope.user = theUser;
 	$scope.orders = theOrders;
-
+	//replace date with format easy to read
+	for (var i= 0; i<$scope.orders.length; i++) {
+		$scope.orders[i].datePlaced = $scope.orders[i].datePlaced.replace(/T.+/,"");
+		// for(var j=0; j<$scope.orders[i].cart.length; j++) {
+		// 	var theProduct = $scope.orders[i].cart[j].product
+		// 	ProductFactory.fetchOne(theProduct)
+		// 	.then(function (product) {
+		// 		theProduct.name = product.name
+		// 	})
+		// 	$scope.orders[i].cart[j].product = theProduct
+		// 	console.log($scope.orders[i].cart[j].product)
+		// }	
+	} 
 })
