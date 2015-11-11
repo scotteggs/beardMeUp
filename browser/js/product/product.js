d8197f27fda85e65f71ba1425d36e820122a9a51
Daniel Somekh <danielsomekh@Daniels-MacBook-Pro.local>
2015-11-11 09:53:07 -0500

    Facebook share button fixed

--- browser/js/product/product.js
+++ browser/js/product/product.js
@@ -81,19 +81,14 @@ app.controller('ProductController', function ($scope, $rootScope, AuthService, t
     }(document, "script", "twitter-wjs"));
     // window.twttr.widgets.load()
 
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    // (function(d, s, id) {
+    //   var js, fjs = d.getElementsByTagName(s)[0];
+    //   if (d.getElementById(id)) return;
+    //   js = d.createElement(s); js.id = id;
+    //   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+    //   fjs.parentNode.insertBefore(js, fjs);
+    // }(document, 'script', 'facebook-jssdk'));
         
-
-
-
-
-
 
     $scope.stream = function() {
         $scope.streaming = !$scope.streaming;
